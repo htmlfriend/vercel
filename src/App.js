e1915a7346bed7e0642925c72a0d55066092071a
Yurii Tym <yuriy.tim@yahoo.com>
2021-01-11 17:01:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,108 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
 
-function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
-  return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
+const App = () => {
+  const [news, setNews] = useState([]);
+  const [searchQuery, setSeachQuery] = useState("react");
+
+  const [url, setUrl] = useState(
+    "https://hn.algolia.com/api/v1/search?query=react"
   );
-}
+  const [loading, setLoading] = useState(false);
 
+  const showLoading = () => (loading ? <h2>Loading ... </h2> : "");
+
+  const searchForm = () => (
+    <form onSubmit={hadleSubmit}>
+      <input type='text' value={searchQuery} onChange={searchFormInput} />
+      <button>Search</button>
+    </form>
+  );
+  // fetch news
+  const fetchNews = (url) => {
+    setLoading(true);
+    fetch(url)
+      // .then(data => data.json())
+      // .then(data => console.log(data))
+      .then((result) => result.json())
+      .then((data) => {
+        setNews(data.hits);
+        setLoading(false);
+      })
+      .catch((error) => console.log(error));
+  };
+  useEffect(() => {
+    fetchNews(url);
+  }, [url]);
+
+  const searchFormInput = (e) => {
+    setSeachQuery(e.target.value);
+  };
+
+  const hadleSubmit = (e) => {
+    e.preventDefault();
+    setUrl(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+  };
+
+  const mapArray = () =>
+    news.map((elem, index) => <p key={index}>{elem.title}</p>);
+
+  return (
+    <div>
+      <h2>News</h2>
+      {showLoading()}
+      {searchForm()}
+      {mapArray()}
+    </div>
+  );
+};
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+
+//   const increment = () => {
+//     setCount(count + 1);
+//   };
+
+//   useEffect(() => {
+//     document.title = `Clicked ${count} times`
+//   })
+
+//   return (
+//     <div>
+//       <h1>Hello CodeSandbox</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//       <button onClick={increment}>Clicked {count} times</button>
+//     </div>
+//   );
+// };
+
+//  class App extends Component {
+//   state ={
+//     count :0
+//   }
+
+//   increment =() => {
+//     this.setState({
+//       count: this.state.count + 1
+//     })
+//   };
+
+//   componentDidMount() {
+//     document.title = `Clicked ${this.state.count} times`
+//   }
+
+//   componentDidUpdate() {
+//     document.title = `Clicked ${this.state.count} times`
+//   }
+//   render() {
+//   return (
+//     <div>
+//       <h1>Hello CodeSandbox</h1>
+//       <h2>Start editing to see some magic happen!</h2>
+//       <button onClick={this.increment}>Clicked {this.state.count} times</button>
+//     </div>
+//   );
+// }
+// }
 export default App;
