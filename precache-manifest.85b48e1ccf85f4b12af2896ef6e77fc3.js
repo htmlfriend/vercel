e2f41beb55ee2bf9fd51b6bb83788badcceedccf
Yurii Tym <yuriy.tim@yahoo.com>
2021-01-25 16:59:57 -0500

    Updates

---
+++ precache-manifest.85b48e1ccf85f4b12af2896ef6e77fc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a0521f545a869693bfa",
+    "url": "/getMovieByReact/static/css/main.57c5ce0f.chunk.css"
+  },
+  {
+    "revision": "4a0521f545a869693bfa",
+    "url": "/getMovieByReact/static/js/main.4a0521f5.chunk.js"
+  },
+  {
+    "revision": "bf5c40ba71b850ad2f27",
+    "url": "/getMovieByReact/static/js/1.bf5c40ba.chunk.js"
+  },
+  {
+    "revision": "71d282a45df6c3bc1abf",
+    "url": "/getMovieByReact/static/js/runtime~main.71d282a4.js"
+  },
+  {
+    "revision": "57594abae23dc4dcd55c96d7890961b2",
+    "url": "/getMovieByReact/index.html"
+  }
+];
